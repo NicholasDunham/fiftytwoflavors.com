5f94beadf42ba7e146fe986649039b844de8f210
Nicholas Dunham <11730795+NicholasDunham@users.noreply.github.com>
2023-09-29 19:04:29 +0000

    Further refinement of basic scaffolding

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,11 @@
-import fs from "fs";
 import Link from "next/link";
-import matter from "gray-matter";
-import { PostMetadata } from "@/components/PostMetadata";
-
-const getPostMetadata = (): PostMetadata[] => {
-  const folder = "posts";
-  const files = fs.readdirSync(folder);
-  const markdownPosts = files.filter((file) => file.endsWith(".md"));
-
-  const posts = markdownPosts.map((fileName) => {
-    const fileContents = fs.readFileSync(`${folder}/${fileName}`, "utf8");
-    const matterResult = matter(fileContents);
-    return {
-      title: matterResult.data.title,
-      date: matterResult.data.date,
-      subtitle: matterResult.data.subtitle,
-      slug: fileName.replace(".md", ""),
-    };
-  });
-
-  return posts;
-};
+import getPostMetadata from "@/components/getPostMetadata";
 
 const HomePage = () => {
   const postMetadata = getPostMetadata();
   const postPreviews = postMetadata.map((post) => {
     return (
-      <li>
+      <li key="{post.slug}">
         <h2>
           <Link href={`/posts/${post.slug}`}>{post.title}</Link>
         </h2>
--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,12 +1,20 @@
 import fs from "fs";
 import Markdown from "markdown-to-jsx";
 import matter from "gray-matter";
+import getPostMetadata from "@/components/getPostMetadata";
 
 const getPostContent = (slug: string) => {
   const folder = "posts/";
   const content = fs.readFileSync(`${folder}${slug}.md`, "utf8");
   const matterResult = matter(content);
   return matterResult;
+};
+
+export const generateStaticParams = async () => {
+  const posts = getPostMetadata();
+  return posts.map((post) => ({
+    slug: post.slug,
+  }));
 };
 
 const PostPage = (props: any) => {
