a72e23c9cef89192fd8c364904d8cdba9b317d85
Nicholas Dunham <11730795+NicholasDunham@users.noreply.github.com>
2023-10-05 20:28:55 +0000

    Add nested recipe type for contentlayer

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,4 +1,48 @@
-import { defineDocumentType, makeSource } from "@contentlayer/source-files";
+import { defineDocumentType, defineNestedType, makeSource } from "@contentlayer/source-files";
+
+const Ingredient = defineNestedType(() => ({
+  name: 'Ingredient',
+  fields: {
+    name: { type: "string", required: true },
+    amount: { type: "string", required: true },
+    optional: { type: "boolean", required: true },
+  },
+}));
+
+const IngredientGroup = defineNestedType(() => ({
+  name: 'IngredientGroup',
+  fields: {
+    name: { type: "string", required: true },
+    ingredients: { type: "list", of: Ingredient, required: true },
+    optional: { type: "boolean", required: true },
+  },
+}));
+
+const Direction = defineNestedType(() => ({
+  name: 'Direction',
+  fields: {
+    instruction: { type: "string", required: true },
+    optional: { type: "boolean", required: true },
+  },
+}));
+
+const DirectionGroup = defineNestedType(() => ({
+  name: 'DirectionGroup',
+  fields: {
+    name: { type: "string", required: true },
+    directions: { type: "list", of: Direction, required: true },
+    optional: { type: "boolean", required: true },
+  },
+}));
+
+const Recipe = defineNestedType(() => ({
+  name: 'Recipe',
+  fields: {
+    name: { type: "string", required: true },
+    ingredients: { type: "list", of: [Ingredient, IngredientGroup], required: true },
+    directions: { type: "list", of: [Direction, DirectionGroup], required: true },
+  },
+}));
 
 export const Post = defineDocumentType(() => ({
   name: "Post",
@@ -9,6 +53,7 @@ export const Post = defineDocumentType(() => ({
     date: { type: "date", required: true },
     tags: { type: "list", of: { type: "string" }, required: true },
     abstract: { type: "markdown", required: true },
+    recipe: { type: "nested", of: Recipe, required: false },
   },
   computedFields: {
     url: {
