11ec8c609d91af1f372b3c0642afe87e8dbab9c6
Nicholas Dunham <11730795+NicholasDunham@users.noreply.github.com>
2023-10-06 21:36:29 +0000

    Minor cleanup

--- components/abbreviations.tsx
+++ components/abbreviations.tsx
@@ -1,5 +1,7 @@
 import { FC } from "react";
 
+/* The word "carboxymethylcellulose" can cause ugly line breaks if not
+ * hyphenated. */
 export const CMC: FC = () => {
   return <span>car&shy;boxy&shy;meth&shy;yl&shy;cel&shy;lu&shy;lose</span>;
 };
