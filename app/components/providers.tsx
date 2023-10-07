b82f88163e103fdac1d98c7ab181143463a3cded
Nicholas Dunham <11730795+NicholasDunham@users.noreply.github.com>
2023-10-07 05:13:35 +0000

    Add light/dark themes

---
+++ app/components/providers.tsx
@@ -0,0 +1,7 @@
+"use client";
+import type { FC, ReactNode } from "react";
+import { ThemeProvider } from "next-themes";
+
+export const Providers: FC<{ children: ReactNode }> = ({ children }) => {
+  return <ThemeProvider attribute="class">{children}</ThemeProvider>;
+};
