17ce8c0eff036b58230e56c4b7dfd24d8fed54f8
Nicholas Dunham <11730795+NicholasDunham@users.noreply.github.com>
2023-10-10 04:11:00 +0000

    Consolidate files

--- app/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="mt-4 border-t border-slate-800 py-4 text-slate-500">
-      <p>Created by Nicholas Dunham.</p>
-    </footer>
-  );
-};
-
-export default Footer;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,45 @@
 import "@/styles/globals.css";
 import { Providers } from "./components/providers";
-import Header from "@/app/components/Header";
-import Footer from "@/app/components/Footer";
+import ThemeButton from "@/app/components/ThemeButton";
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "@/public/img/Logo.svg";
 
 export const metadata = {
   title: "Fifty-two Flavors",
   description: "One year of home ice cream recipes.",
+};
+
+const Branding = () => {
+  return (
+    <>
+      <Link href="/" className="flex space-x-2">
+        <Image src={Logo} alt="Fifty-two Flavors" width={20} height={20} />
+        <h1 className="text-2xl font-medium">Fifty-two Flavors</h1>
+      </Link>
+    </>
+  );
+};
+
+const Header = () => {
+  return (
+    <nav className="max-w-5xl mx-auto">
+      <div className="flex justify-between h-16">
+        <div className="flex justify-between items-center w-full">
+          <Branding />
+          <ThemeButton />
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+const Footer = () => {
+  return (
+    <footer className="mt-4 border-t border-slate-800 py-4 text-slate-500">
+      <p>Created by Nicholas Dunham.</p>
+    </footer>
+  );
 };
 
 export default function RootLayout({
